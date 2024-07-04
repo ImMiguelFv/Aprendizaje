3f5b8cb81925a76686bee27a27d9715194d099ae
ImMiguelFv <mferronvinuela@gmail.com>
2024-07-04 10:54:00 +0200

    Learning Techs

---
+++ typeScript/.ts
@@ -0,0 +1,11 @@
+let persona = {
+    nombre: 'Tony',
+    edad: 45,
+    direccion: {
+        calle: 'Primera',
+        pais: 'Colombia',
+        estado: 'Cundinamarca'
+    }
+}
+
+console.log(persona);
